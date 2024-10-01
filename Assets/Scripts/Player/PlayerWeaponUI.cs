3d7c9fbdd8d92b8fc6de363c990bb86ff8144d87
ptrefall <ptrefall@gmail.com>
2024-10-01 14:06:53 +0200

    Cleaned up Player Weapon UI

--- Assets/Scripts/Player/PlayerWeaponUI.cs
+++ Assets/Scripts/Player/PlayerWeaponUI.cs
@@ -2,83 +2,116 @@ using FPSDemo.Target;
 using FPSDemo.Weapons;
 using TMPro;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace FPSDemo.Player
 {
 
 	public class WeaponUI : MonoBehaviour
 	{
-		[SerializeField] GameObject crosshairGameObject;
-		PlayerWeaponController weaponController;
-		Player player;
-		HealthSystem healthSystem;
-		Weapon equippedWeapon;
-		RectTransform reticleTransform;
-		float maxAngleSpread = 15f;
-		float maxReticleSize = 500;
-		float minReticleSize = 100;
+        // ========================================================= INSPECTOR FIELDS
 
-		[SerializeField] TextMeshProUGUI ammoText = null;
+        [SerializeField] private RectTransform _reticleTransform;
+        [SerializeField] private TextMeshProUGUI _ammoText = null;
 
-		void Awake()
+		[Header("Dependencies")]
+        [SerializeField] private PlayerWeaponController _weaponController;
+        [SerializeField] private Player _player;
+        [SerializeField] private HealthSystem _healthSystem;
+
+
+        // ========================================================= PRIVATE FIELDS
+
+        private Weapon _equippedWeapon;
+		private float _maxAngleSpread = 15f;
+		private float _maxReticleSize = 500;
+		private float _minReticleSize = 100;
+
+
+        // ========================================================= UNITY METHODS
+
+        private void OnValidate()
+        {
+            if (_weaponController == null)
+            {
+                _weaponController = GetComponent<PlayerWeaponController>();
+            }
+
+            if (_player == null)
+            {
+                _player = GetComponent<Player>();
+            }
+
+            if (_healthSystem == null)
+            {
+                _healthSystem = GetComponent<HealthSystem>();
+            }
+        }
+
+        void Awake()
 		{
-			player = GetComponent<Player>();
-			weaponController = GetComponent<PlayerWeaponController>();
-			healthSystem = GetComponent<HealthSystem>();
-			equippedWeapon = weaponController.EquippedWeapon;
-			reticleTransform = crosshairGameObject.GetComponent<RectTransform>();
+			_equippedWeapon = _weaponController.EquippedWeapon;
 		}
 
 		void Start()
 		{
-			UpdateAmmoText();
+			OnUpdateAmmoText();
 		}
-
 
 		private void OnEnable()
 		{
-			healthSystem.OnDeath += OnDeath;
-			weaponController.OnUpdate += UpdateCrosshair;
-			weaponController.OnFire += UpdateAmmoText;
-			weaponController.OnReload += UpdateAmmoText;
+			_healthSystem.OnDeath += OnDeath;
+			_weaponController.OnUpdate += OnUpdateCrosshair;
+			_weaponController.OnFire += OnUpdateAmmoText;
+			_weaponController.OnReload += OnUpdateAmmoText;
 		}
 
 		private void OnDisable()
 		{
-			healthSystem.OnDeath -= OnDeath;
-			weaponController.OnUpdate -= UpdateCrosshair;
-			weaponController.OnFire -= UpdateAmmoText;
-			weaponController.OnReload -= UpdateAmmoText;
+			_healthSystem.OnDeath -= OnDeath;
+			_weaponController.OnUpdate -= OnUpdateCrosshair;
+			_weaponController.OnFire -= OnUpdateAmmoText;
+			_weaponController.OnReload -= OnUpdateAmmoText;
 		}
 
-		void OnDeath()
+
+        // ========================================================= CALLBACKS
+
+        private void OnDeath()
 		{
 			SetCrosshairVisibility(false);
 		}
 
-		void UpdateCrosshair()
+        private void OnUpdateCrosshair()
 		{
-			UpdateReticleSize(weaponController.CurrentOverallAngleSpread);
-			SetCrosshairVisibility(!player.IsAiming);
+			UpdateReticleSize(_weaponController.CurrentOverallAngleSpread);
+			SetCrosshairVisibility(_player.IsAiming == false);
 		}
 
-		void UpdateReticleSize(float currentAngleSpread)
+        private void OnUpdateAmmoText()
+        {
+            if (_ammoText != null)
+            {
+				_ammoText.text = $"{_equippedWeapon.currentAvailableAmmo} / {(_weaponController.AvailableMagazines * _equippedWeapon.maxAmmoInMagazine)}";
+            }
+        }
+
+
+        // ========================================================= TICK
+
+        private void UpdateReticleSize(float currentAngleSpread)
 		{
-			float currentSpreadToReticleSize = Mathf.Lerp(minReticleSize, maxReticleSize, currentAngleSpread / maxAngleSpread);
-			reticleTransform.sizeDelta = new Vector2(currentSpreadToReticleSize, currentSpreadToReticleSize);
+			var currentSpreadToReticleSize = Mathf.Lerp(_minReticleSize, _maxReticleSize, currentAngleSpread / _maxAngleSpread);
+
+			_reticleTransform.sizeDelta = new Vector2(currentSpreadToReticleSize, currentSpreadToReticleSize);
 		}
 
-		void UpdateAmmoText()
-		{
-			if (ammoText != null)
-			{
-				ammoText.text = equippedWeapon.currentAvailableAmmo.ToString() + " / " + (weaponController.AvailableMagazines * equippedWeapon.maxAmmoInMagazine).ToString();
-			}
-		}
 
-		void SetCrosshairVisibility(bool visible)
+        // ========================================================= SETTERS
+
+        private void SetCrosshairVisibility(bool visible)
 		{
-			crosshairGameObject.SetActive(visible);
+			_reticleTransform.gameObject.SetActive(visible);
 		}
 	}
 }
