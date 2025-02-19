1e96f4a04ac0cdd1b1fbb5c5a1e817c89a0f1a9a
ptrefall <ptrefall@gmail.com>
2025-02-19 07:20:00 +0100

    Started adding tactical probes and made the tactical position generator find probes in the scene.

---
+++ Assets/Scripts/Editor/TacticalPositionGeneratorEditor.cs
@@ -0,0 +1,71 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace FPSDemo.NPC.Utilities
+{
+	[CustomEditor(typeof(TacticalPositionGenerator))]
+	public class TacticalPositionGeneratorEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			TacticalPositionGenerator posGenerator = (TacticalPositionGenerator)target;
+
+			if (GUILayout.Button("Generate tactical positions for AI"))
+			{
+				if (posGenerator.TacticalPositionData == null)
+				{
+					Debug.LogError("TacticalPositionData is missing!");
+					return;
+				}
+
+                if (posGenerator.UseHandplacedTacticalProbes)
+                {
+                    var probes = FindObjectsByType<TacticalProbe>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+                    if (probes != null && probes.Length > 0)
+                    {
+                        posGenerator.AddTacticalProbes(clearPositions:true, probes);
+                        if (probes.Length == 1)
+                        {
+                            Debug.Log($"Added 1 tactical probe from scene!");
+                        }
+                        else
+                        {
+                            Debug.Log($"Added {probes.Length} tactical probes from scene!");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("Found 0 tactical probes in scene!");
+                        return;
+                    }
+                }
+
+                if (posGenerator.GenerateAutoProbeGrid)
+                {
+                    posGenerator.GenerateTacticalPositions(clearPositions: posGenerator.UseHandplacedTacticalProbes == false);
+                }
+
+                EditorUtility.SetDirty(posGenerator.TacticalPositionData);
+				AssetDatabase.SaveAssetIfDirty(posGenerator.TacticalPositionData);
+			}
+
+            if (GUILayout.Button("Clear generated tactical positions for AI"))
+            {
+                if (posGenerator.TacticalPositionData != null && posGenerator.TacticalPositionData.Positions.Count > 0)
+                {
+                    Debug.Log("TacticalPositionData cleared!");
+                    posGenerator.TacticalPositionData.Positions.Clear();
+                    EditorUtility.SetDirty(posGenerator.TacticalPositionData);
+                    AssetDatabase.SaveAssetIfDirty(posGenerator.TacticalPositionData);
+                    return;
+                }
+                else
+                {
+                    Debug.Log("TacticalPositionData already cleared.");
+                }
+            }
+
+            base.OnInspectorGUI();
+		}
+	}
+}
